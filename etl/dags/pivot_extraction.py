fe218b3d3c07e5e5b04ca213cdb77b1adc0a3f44
Vitor Beck <toorugoo@gmail.com>
2022-12-07 09:22:33 -0300

    extract and download workintg

--- etl/dags/pivot_extraction.py
+++ etl/dags/pivot_extraction.py
@@ -22,7 +22,7 @@ with DAG(
     data_url = 'https://www.gov.br/anp/pt-br/centrais-de-conteudo/dados-estatisticos/de/vdpb/vendas-combustiveis-m3.xls'
     file_name = data_url.split('/')[-1]
     download = BashOperator(task_id = 'download_pivot_data', bash_command=f"curl --create-dirs -O --output-dir /home/airflow/data/ {data_url}")
-    ods_converter = BashOperator(task_id = 'convert_xls_ods', bash_command=f"soffice --headless --convert-to ods --outdir /home/airflow/data {file_name}")
+    ods_converter = BashOperator(task_id = 'convert_xls_ods', bash_command=f"soffice --headless --convert-to ods --outdir /home/airflow/data {file_name}", queue='libreoffice')
     # save = PythonOperator
 
     download >> ods_converter
