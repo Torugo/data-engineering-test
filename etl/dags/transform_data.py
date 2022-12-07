13cd430d6a57151262f64170133e3aab4c0b640d
Vitor Beck <toorugoo@gmail.com>
2022-12-07 13:10:32 -0300

    saving extracted data as parquet in partitions

--- etl/dags/transform_data.py
+++ etl/dags/transform_data.py
@@ -13,17 +13,81 @@ default_args = {
 
 
 def transform_save(**kwargs):
+    def get_timestamp():
+        from datetime import datetime, timezone, timedelta
+
+        now = datetime.now(timezone.utc)
+        epoch = datetime(1970, 1, 1, tzinfo=timezone.utc) # use POSIX epoch
+        posix_timestamp_millis = (now - epoch) // timedelta(milliseconds=1)
+        return posix_timestamp_millis
+
     import pandas as pd
     from pathlib import Path
     dest_dir = Path('/root/airflow/data/data-lake')
-    file_name = '/root/airflow/data/vendas-combustiveis-m3.ods'
-    df = pd.read_excel(file_name, sheet_name='DPCache_m3 -1')
-    df = df[df.COMBUSTÍVEL != 'ETANOL HIDRATADO (m3)']
-    df = df.drop('REGIÃO', axis=1)
 
+    df_parquet = pd.read_parquet('/root/airflow/data/data-lake/raw.parquet')
+    df_parquet = df_parquet.fillna(0)
+    df_parquet = df_parquet.rename(columns={"COMBUSTÍVEL": "product", "ESTADO": "uf", "UNIDADE": "unit", "ANO": 'ano'})
+    df_parquet = pd.melt(df_parquet,
+                            id_vars=['product', 'ano', 'unit', 'uf', 'TOTAL'],
+                            var_name='mes',
+                            value_name='volume') 
+
+    df_parquet['product'] = df_parquet['product'].str.replace('m3', '', regex=True) 
+    df_parquet['product'] = df_parquet['product'].str.replace('\(', '', regex=True) 
+    df_parquet['product'] = df_parquet['product'].str.replace('\)', '', regex=True) 
+    df_parquet['product'] = df_parquet['product'].str.rstrip().str.lstrip()
+    df_parquet['product'] = df_parquet['product'].str.replace(' ', '_', regex=True) 
+
+    df_parquet['mes'] = df_parquet['mes'].replace({'Jan': '01', 'Fev': '02', 'Mar': '03',
+                                                'Abr': '04', 'Mai': '05', 'Jun': '06',
+                                                'Jul': '07', 'Ago': '08', 'Set': '09',
+                                                'Out': '10', 'Nov': '11', 'Dez': '12'})
+    df_parquet['uf'] = df_parquet['uf'].replace({"ACRE":"AC",
+                                            "ALAGOAS":"AL",
+                                            "AMAZONAS":"AM",
+                                            "AMAPÁ":"AP",
+                                            "BAHIA":"BA",
+                                            "CEARÁ":"CE",
+                                            "DISTRITO FEDERAL":"DF",
+                                            "ESPÍRITO SANTO":"ES",
+                                            "GOIÁS":"GO",
+                                            "MARANHÃO":"MA",
+                                            "MINAS GERAIS":"MG",
+                                            "MATO GROSSO DO SUL":"MS",
+                                            "MATO GROSSO":"MT",
+                                            "PARÁ":"PA",
+                                            "PARAÍBA":"PB",	
+                                            "PERNAMBUCO":"PE",
+                                            "PIAUÍ":"PI",
+                                            "PARANÁ":"PR",
+                                            "RIO DE JANEIRO":"RJ",
+                                            "RIO GRANDE DO NORTE":"RN",
+                                            "RONDÔNIA":"RO",
+                                            "RORAIMA":"RR",
+                                            "RIO GRANDE DO SUL":"RS",
+                                            "SANTA CATARINA":"SC",
+                                            "SERGIPE":"SE",
+                                            "SÃO PAULO":"SP",
+                                            "TOCANTINS":"TO"})
+    df_parquet['timestamp'] = get_timestamp()
+
+    df_oil = df_parquet[df_parquet['product'] == 'ÓLEO_DIESEL']
+    df_derivate =  df_parquet[df_parquet['product'] != 'ÓLEO_DIESEL']
+
+    from pathlib import Path
+    dest_dir = Path('/root/airflow/data/data-lake/silver')    
     dest_dir.mkdir(parents=True, exist_ok=True)
+    
+    ##VALIDATION
 
-    df.to_parquet(str(dest_dir) + "/"  +'raw.parquet')
+    df_derivate = df_derivate.drop(['ano', 'mes', 'TOTAL'], axis=1)
+    df_oil = df_oil.drop(['ano', 'mes', 'TOTAL'], axis=1)
+
+
+    df_derivate.to_parquet('dest_dir' + '/' + 'derivate.parquet', partition_cols=['product', 'year_month', ])
+    df_oil.to_parquet('dest_dir' + '/' + 'oil.parquet', partition_cols=['product', 'year_month', ])
+
 
 with DAG(
     'transform_etl_dag',
